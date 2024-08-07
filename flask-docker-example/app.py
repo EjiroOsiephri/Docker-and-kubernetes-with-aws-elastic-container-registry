0bcf70e8e46fee9f1121da23465d333ab4c135d1
ejiro osiephri <osiephriejiro765@gmail.com>
2024-08-08 00:13:01 +0100

    feats/setup docker on python, complete

--- flask-docker-example/app.py
+++ flask-docker-example/app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from http.server import SimpleHTTPRequestHandler, HTTPServer
 
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, Docker!'
+class MyHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"Hello, Docker!")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    server = HTTPServer(('0.0.0.0', 80), MyHandler)
+    print("Starting server on port 80...")
+    server.serve_forever()
